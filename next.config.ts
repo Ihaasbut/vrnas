f7d6fc389da22ba198f81d204bd37b8461499ec5
ihaasbut <ihaasbut@gmail.com>
2026-06-05 14:07:05 +0300

    VRN-4 внесла правки после merge

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,8 @@ const nextConfig: NextConfig = {
     reactCompiler: true,
     sassOptions: {
         additionalData: `
-          @use "@/assets/styles/mixins.scss" as *,
-          @use "${path
-              .join(process.cwd(), "src/assets/styles/colors.scss")
-              .replace(/\\/g, "/")}" as *;
+          @use "@/assets/styles/mixins.scss" as *;
+          @use "@/assets/styles/colors.scss" as *;
         `,
     },
 };
--- src/pages/home/HomeContent.tsx
+++ src/pages/home/HomeContent.tsx
@@ -1,8 +1,10 @@
+import Button from "@/components/button/Button";
 import styles from "./HomeContent.module.scss";
 function HomeContent() {
     return (
         <div>
             <h1 className={styles.title}>Hello World</h1>
+            <Button />
         </div>
     );
 }
