014b9fea1b728cd0c940c27efa84b5f01a431eef
twnnkle <arluncov@yandex.ru>
2026-06-05 13:41:08 +0300

    VRN-11: Подключил миксины. Правки

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  sassOptions: {
+    additionalData: `
+      @use "@/assets/styles/mixins.scss" as *;
+    `,
+  },
 };
 
 export default nextConfig;
--- src/pages/home/HomeContent.tsx
+++ src/pages/home/HomeContent.tsx
@@ -1,7 +1,9 @@
+import Button from "@/components/button/Button";
+
 function HomeContent() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <Button />
         </div>
     );
 }
