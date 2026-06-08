9dcb7f2416606a2dd9249748fc3ce8ce6368f06e
ihaasbut <ihaasbut@gmail.com>
2026-06-08 12:41:30 +0300

    VRN-10 Сделала 2 класса для цветных бордеров, установила cn

--- src/pages/home/HomeContent.tsx
+++ src/pages/home/HomeContent.tsx
@@ -1,10 +1,12 @@
 import Button from "@/components/button/Button";
 import styles from "./HomeContent.module.scss";
 
+import cn from "classnames";
+
 function HomeContent() {
     return (
         <div>
-            <h1 className={styles.title}>Hello World</h1>
+            <h1 className={cn(styles.title, "border-linear-2")}>Hello World</h1>
             <Button />
         </div>
     );
