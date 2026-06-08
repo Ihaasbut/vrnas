adffcabef9836449479e577701371f3a490299e7
ihaasbut <ihaasbut@gmail.com>
2026-06-08 11:15:35 +0300

    VRN-10 сделала класс для разноцветного текста

--- src/pages/home/HomeContent.tsx
+++ src/pages/home/HomeContent.tsx
@@ -4,7 +4,7 @@ import styles from "./HomeContent.module.scss";
 function HomeContent() {
     return (
         <div>
-            <h1 className={styles.title}>Hello World</h1>
+            <h1 className="text-linear">Hello World</h1>
             <Button />
         </div>
     );
