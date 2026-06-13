83d0e41beadf4e37fc4df385b20e8e1b7a334f5c
ihaasbut <ihaasbut@gmail.com>
2026-06-13 17:33:33 +0300

    VRN-6 Сделала 3 варианта кнопок, декстоп = мобилка по дизайну

--- src/pages/home/HomeContent.tsx
+++ src/pages/home/HomeContent.tsx
@@ -1,13 +1,29 @@
 import Button from "@/components/button/Button";
+import Typography from "@/components/typography/Typography";
 import styles from "./HomeContent.module.scss";
-
-import cn from "classnames";
 
 function HomeContent() {
     return (
         <div>
             <h1 className="text-linear">Hello World</h1>
-            <Button />
+
+            <div className={styles.buttons}>
+                <Button variant="fill">
+                    <Typography variant="button-1" as={"span"}>
+                        Get started
+                    </Typography>
+                </Button>
+                <Button variant="outline">
+                    <Typography variant="button-1" as={"span"}>
+                        learn more
+                    </Typography>
+                </Button>
+                <Button variant="ghost">
+                    <Typography variant="button-2" as={"span"}>
+                        Contacts
+                    </Typography>
+                </Button>
+            </div>
         </div>
     );
 }
