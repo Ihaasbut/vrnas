923481e9c44daccaea81d85c6859be0bea0163ec
ihaasbut <ihaasbut@gmail.com>
2026-06-18 11:19:17 +0300

    VRN-22: Сделала хлебные крошки

--- src/pages/home/HomeContent.tsx
+++ src/pages/home/HomeContent.tsx
@@ -5,50 +5,62 @@ import Typography from "@/components/typography/Typography";
 import styles from "./HomeContent.module.scss";
 import Container from "@/components/container/Container";
 import { useClientBreakpoint } from "@/hooks/useClientBreakpoint";
+import Breadcrumbs from "@/components/breadcrumbs/Breadcrumbs";
 
 function HomeContent() {
    const { isDesktop } = useClientBreakpoint();
 
+   const breadcrumbs = [
+      {
+         title: "Home",
+         link: "/",
+      },
+      {
+         title: "About",
+         link: "/about",
+      },
+      {
+         title: "Contact",
+         link: "/contact",
+      },
+   ];
+
    return (
       <div>
-         <h1 className="text-linear">Hello World</h1>
+         <Container>
+            <h1 className="text-linear">Hello World</h1>
 
-         <div className={styles.buttons}>
-            <Button variant="fill" onClick={() => {}}>
-               <Typography variant="button-1" as={"span"}>
-                  Get started
-               </Typography>
-            </Button>
+            {/* <div className={styles.buttons}>
+               <Button variant="fill" onClick={() => {}}>
+                  <Typography variant="button-1" as={"span"}>
+                     Get started
+                  </Typography>
+               </Button>
 
-            <Button variant="outline" onClick={() => {}}>
-               <Typography variant="button-1" as={"span"}>
-                  learn more
-               </Typography>
-            </Button>
+               <Button variant="outline" onClick={() => {}}>
+                  <Typography variant="button-1" as={"span"}>
+                     learn more
+                  </Typography>
+               </Button>
 
-            <Button variant="ghost" onClick={() => {}}>
-               <Typography variant="button-2" as={"span"}>
-                  Contacts
-               </Typography>
-            </Button>
+               <Button variant="ghost" onClick={() => {}}>
+                  <Typography variant="button-2" as={"span"}>
+                     Contacts
+                  </Typography>
+               </Button>
 
-            <div style={{ backgroundColor: "red" }}>
-               <Container inlinePadding={!isDesktop ? "0" : "120px 0"}>
-                  <div style={{ backgroundColor: "blue" }}>
-                     <h1>Hello World</h1>
-                  </div>
-               </Container>
-            </div>
+               <Button variant="ghost" onClick={() => {}} isSkipPadding>
+                  <div
+                     style={{
+                        width: "40px",
+                        height: "40px",
+                     }}
+                  ></div>
+               </Button>
+            </div> */}
 
-            <Button variant="ghost" onClick={() => {}} isSkipPadding>
-               <div
-                  style={{
-                     width: "40px",
-                     height: "40px",
-                  }}
-               ></div>
-            </Button>
-         </div>
+            <Breadcrumbs breadcrumbs={breadcrumbs} />
+         </Container>
       </div>
    );
 }
